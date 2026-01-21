76fd977e8f7bcee8a345bf1c828c171df1082cd4
Muhammad Jawad <112897469+muhammadJawadm@users.noreply.github.com>
2026-01-21 11:16:32 +0500

    logo import error resolve

--- src/components/Seller/SellerSidebar.jsx
+++ src/components/Seller/SellerSidebar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FaHome, FaList, FaGavel, FaBalanceScale, FaLock, FaBars, FaTimes, FaHandshake } from 'react-icons/fa';
 import SplashLogo from '../../assets/Splashlogo2.png';
-import Ailogo from '../../assets/group.png';
+import Ailogo from '../../assets/Group.png';
 import AIChat from '../common/AIChat';
 
 // Create a global state for sidebar collapse
